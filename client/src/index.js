956a0110b299cc51490ec68c49e7c9a863661e18
unknown <oasis@LAPTOP-NPA63NIP.mit.edu>
2023-01-23 03:21:49 -0500

    Added a mock version of the navbar.
    Going to finish the homepage tomorrow.

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react'
 
 import App from "./components/App.js";
+
 
 // renders React Component "Root" into the DOM element with ID "root"
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ChakraProvider> 
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  </ChakraProvider>
+
 );
 
 // allows for live updating
